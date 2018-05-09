9b4b5c71123c48be2bf282806531e693c9bb0058
duc anh <ducanhdhtb@gmail.com>
2018-05-10 02:18:47 +0700

    coolfix

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-include_once('library/connect.php');
+include_once  develop sua no day
 switch($_GET['controller']){
 	case 'user': include_once('controllers/user.php');
 	break;
 	case 'product': include_once('controllers/product.php');
 	break;
 }
+
 ?>
